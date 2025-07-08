b248a0c40554a3e129f922a00487266c24d78fd0
AtillaTokmak <atilla1tokmak2@gmail.com>
2025-07-08 22:39:31 +0300

    atilla08/07US_006-TC_002V1

--- src/test/java/tests/atilla/US_006.java
+++ src/test/java/tests/atilla/US_006.java
@@ -1,5 +1,6 @@
 package tests.atilla;
 
+import org.testng.Assert;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.Test;
 import pages.userPages;
@@ -14,11 +15,32 @@ public class US_006 {
         Driver.getDriver().get(ConfigReader.getProperty("url"));
         ReusableMethods.bekle(3);
         userPages userPages = new userPages();
-
         userPages.listingButton.click();
-
+        ReusableMethods.bekle(3);
     }
 
+    @Test
+    public void TC_002() {
+        String url = ConfigReader.getProperty("househeavenproperties.url");
+        Driver.getDriver().get(url);
 
+        ReusableMethods.bekle(10);
 
+        String expectedTitle = "Properties";
+        String actualTitle = Driver.getDriver().getTitle();
+
+        if (actualTitle.contains(expectedTitle)) {
+            System.out.println("Sayfa başarılı şekilde açıldı. Title: " + actualTitle);
+        } else {
+            System.out.println("Sayfa açılamadı veya title beklendiği gibi değil.");
+            System.out.println("Beklenen: " + expectedTitle);
+            System.out.println("Gerçek: " + actualTitle);
+            Assert.fail("Title kontrolü başarısız.");
+        }
+    }
+
+    @AfterTest
+    public void tearDown() {
+        Driver.quitDriver();
+    }
 }

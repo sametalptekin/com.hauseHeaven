11f44ed86ff20fcfc988843e8f91ef63c6097156
AtillaTokmak <atilla1tokmak2@gmail.com>
2025-07-08 22:12:58 +0300

    atilla08/07US_008-TC_001V1

--- src/test/java/tests/atilla/US_006.java
+++ src/test/java/tests/atilla/US_006.java
@@ -1,4 +1,24 @@
 package tests.atilla;
 
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.Test;
+import pages.userPages;
+import utilities.ConfigReader;
+import utilities.Driver;
+import utilities.ReusableMethods;
+
 public class US_006 {
+
+    @Test
+    public void TC_001(){
+        Driver.getDriver().get(ConfigReader.getProperty("url"));
+        ReusableMethods.bekle(3);
+        userPages userPages = new userPages();
+
+        userPages.listingButton.click();
+
+    }
+
+
+
 }

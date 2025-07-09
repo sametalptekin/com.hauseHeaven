824f953990b1942a13ba82c347e31894be0a353d
enesgulsenn <enesgulsen@yahoo.com>
2025-07-09 17:44:22 +0300

    enes09/07-US_002-TC_001_TC_002_V1

--- src/test/java/pages/userPages.java
+++ src/test/java/pages/userPages.java
@@ -22,6 +22,38 @@ public class userPages {
     @FindBy(xpath = "//a[normalize-space(text())='Listing']")
     public WebElement listingButton;
 
+    @FindBy(xpath = "//ul[@class='nav-menu']//a[contains(text(), 'Home')]")
+    public WebElement homeButton;
+
+    @FindBy(xpath = "//ul[@class='nav-menu']//a[contains(text(), 'Projects')]")
+    public WebElement projectsButton;
+
+    @FindBy(xpath = "//ul[@class='nav-menu']//a[contains(text(), 'Agents')]")
+    public WebElement agentsButton;
+
+    @FindBy(xpath = "//ul[@class='nav-menu']//a[contains(text(), 'Blog')]")
+    public WebElement blogButton;
+
+    @FindBy(xpath = "//ul[@class='nav-menu']//a[contains(text(), 'Contact')]")
+    public WebElement contactButton;
+
+    @FindBy(xpath = "//ul[@class='nav-menu']//a[contains(text(), 'Sign Up')]")
+    public WebElement signupButton;
+
+    @FindBy(xpath = "//a[@class='text-success']")
+    public WebElement addPropertyButton;
+
+    @FindBy(xpath = "//img[@class='logo']")
+    public WebElement logoButton;
+
+
+
+
+
+
+
+
+
 
 
 

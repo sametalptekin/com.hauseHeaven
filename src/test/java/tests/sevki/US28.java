a9b6fa92353b4485372b164b5ece85e4ecde0536
sevkieren <erensevki875@gmail.com>
2025-07-10 23:19:41 +0300

    Version_8

---
+++ src/test/java/tests/sevki/US28.java
@@ -0,0 +1,29 @@
+package tests.sevki;
+
+public class US28  {
+
+        public static void main(String[] args) {
+            System.out.println("aaa");
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}

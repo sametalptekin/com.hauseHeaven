5ba637a78184d6fc2abe9dc31eb4c4be9ff690c0
mertcan <dikmenmertcan@gmail.com>
2025-07-10 17:51:44 +0300

    mertcan10/07-US_024-TC_001V1

---
+++ src/test/java/tests/mertcan/US_024.java
@@ -0,0 +1,30 @@
+package tests.mertcan;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import pages.userPages;
+import utilities.ConfigReader;
+import utilities.Driver;
+import utilities.ReusableMethods;
+
+public class US_024 {
+
+    @Test
+    public void TC_001(){
+        Driver.getDriver().get(ConfigReader.getProperty("url"));
+        ReusableMethods.bekle(2);
+        userPages userPages=new userPages();
+
+        userPages.signinButton.click();
+
+        userPages.loginFormEmail.sendKeys(ConfigReader.getProperty("user"));
+        userPages.loginFormPassword.sendKeys(ConfigReader.getProperty("user_pass"));
+        userPages.loginButton.click();
+
+        ReusableMethods.bekle(2);
+        Assert.assertTrue(userPages.logoutButton.isDisplayed());
+        Driver.quitDriver();
+    }
+
+
+}

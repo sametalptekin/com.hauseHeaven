2a8a8e7fb9704133c82fccdae351ec8e065f65ea
sevda <sevdakanik07@gmail.com>
2025-07-14 16:27:49 +0300

    Sevda14/07/2025/v01

--- src/test/java/tests/sevda/US_001.java
+++ src/test/java/tests/sevda/US_001.java
@@ -17,8 +17,29 @@ public class US_001 {
         Driver.getDriver().get(ConfigReader.getProperty("url"));
 
         ReusableMethods.bekle(2);
+        userPages userPages=new userPages();
 
         Assert.assertTrue(Driver.getDriver().getTitle().toLowerCase().contains("home") || Driver.getDriver().getCurrentUrl().contains("hauseheaven"));
+
+        Assert.assertTrue(userPages.signinButton.isDisplayed());
+
+        Assert.assertTrue(userPages.signupButton.isDisplayed());
+
+        Assert.assertTrue(userPages.homeButton.isDisplayed());
+
+        Assert.assertTrue(userPages.projectsButton.isDisplayed());
+        Assert.assertTrue(userPages.agentsButton.isDisplayed());
+
+        Assert.assertTrue(userPages.blogButton.isDisplayed());
+        Assert.assertTrue(userPages.contactButton.isDisplayed());
+        Assert.assertTrue(userPages.listingButton.isDisplayed());
+        Driver.quitDriver();
+
+    }
+    @Test
+    public void TC_002 ()
+    {
+
 
 
 
